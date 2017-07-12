9605abf217ab3160808c5a8bb69497dadb425911
grain111 <grain111@gmail.com>
2017-07-12 06:21:03 +0200

    Next

--- Block.py
+++ Block.py
@@ -1,12 +1,12 @@
 class Block(object):
     
-    shapes = {"I":[[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]],
-              "T":[[1,1,1,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]],
-              "O":[[1,1,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
-              "L":[[1,0,0,0],[1,0,0,0],[1,1,0,0],[0,0,0,0]],
-              "J":[[0,1,0,0],[0,1,0,0],[1,1,0,0],[0,0,0,0]],
-              "S":[[0,1,1,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
-              "Z":[[1,1,0,0],[0,1,1,0],[0,0,0,0],[0,0,0,0]],
+    shapes = {"I":[[1],[1],[1],[1]],
+              "T":[[1,1,1],[0,1,0]],
+              "O":[[1,1],[1,1]],
+              "L":[[1,0],[1,0],[1,1]],
+              "J":[[0,1],[0,1],[1,1]],
+              "S":[[0,1,1],[1,1,0]],
+              "Z":[[1,1,0],[0,1,1]],
                    }
     
     def __init__(self, pos, clr, shp):
@@ -21,16 +21,17 @@ class Block(object):
         for i, row in enumerate(self.shp):
             for j, item in enumerate(row):
                 if item == 1:
+                    fill(self.clr[0], self.clr[1], self.clr[2])
                     rect(self.pos[0] + j * 20, self.pos[1] + i * 20, 20, 20)
                     
     def check_boundries(self):
         for i, row in enumerate(self.shp):
             for j, item in enumerate(row):
-                if self.pos[1] + (i - 1) * 20 > height:
-                    self.pos[1] = height - (i - 1) * 20
+                if self.pos[1] + (i + 1) * 20 > height:
+                    self.pos[1] = height - (i + 1) * 20
                     self.active = False
                            
-    def update(self, dt):
+    def update(self, dt, blocks):
         if self.active:
             self.pos[1] += dt
             self.check_boundries()
--- Tetris.pyde
+++ Tetris.pyde
@@ -2,8 +2,7 @@ from Block import Block
 
 blocks = []
 
-blocks.append(Block([400/2, 450], (50, 0, 50), "Z"))
-blocks.append(Block([200/2, 450], (50, 0, 50), "T"))
+blocks.append(Block([400/2, 450], (150, 0, 150), "Z"))
 
 def setup():
     size(400, 700)
@@ -12,7 +11,7 @@ def draw():
     background(51)
     for bl in blocks:
         bl.show()
-        bl.update(1)
-        if not bl.active:
-            blocks.append(Block([400/2, 450], (50, 0, 50), "I"))
+        bl.update(1, blocks)
+        if not blocks[-1].active:
+            blocks.append(Block([400/2, 450], (150, 0, 150), "I"))
     
